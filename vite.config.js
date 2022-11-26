1fa0c234e498377a30b566923d410bd8334d349a
Lucas10 <63660708+Lucas104-alt@users.noreply.github.com>
2022-11-26 16:44:35 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import legacy from '@vitejs/plugin-legacy'
-import { defineConfig } from 'vite'
-import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
-
-
-export default defineConfig({
-    plugins: [NgmiPolyfill()],
-  });
