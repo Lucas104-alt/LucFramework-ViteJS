1b88d445e28a45202c11db2f95ae7be8578babb0
Lucas10 <63660708+Lucas104-alt@users.noreply.github.com>
2022-11-26 15:54:07 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import { dirname, join } from "path"
+import {fileURLToPath} from "url"
+
+
+const app = express()
+
+app.get("/", (req, res) => res.render("index", )) //LucFramework -- Index.EJS
+
+app.get("/Hello", (req, res) => res.render("Hello")) //LucFramework -- Hello.EJS
+
+app.get("/allgood", (req, res) => res.render("allgood", )) //LucFramework -- AllGood.EJS
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(join(__dirname, "views"))
+
+app.set("views", join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.use(express.static(join(__dirname, "public")))
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`The Server is ready on port http://localhost:${PORT}`)
+
+});
