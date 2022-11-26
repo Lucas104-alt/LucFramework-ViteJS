b82ca68b8d0300ffe47df07dba843adaa6706843
Lucas10 <63660708+Lucas104-alt@users.noreply.github.com>
2022-11-26 16:43:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import nodePolyfills from 'rollup/plugin-inject';
+
 import express from "express"
 import { dirname, join } from "path"
 import {fileURLToPath} from "url"
@@ -13,12 +13,11 @@ app.get("/Hello", (req, res) => res.render("Hello")) //LucFramework -- Hello.EJS
 app.get("/allgood", (req, res) => res.render("allgood", )) //LucFramework -- AllGood.EJS
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(join(__dirname, "views"))
+console.log(join("views"))
 
-app.set("views", join(__dirname, "views"))
+app.set("views", join("views"))
 app.set("view engine", "ejs")
 
-app.use(express.static(join(__dirname, "public")))
 
 
 
